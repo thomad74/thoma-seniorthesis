b96085d2a6a4ec654c5a14658994563669bca66e
Dillon Thoma <dillon@Dillons-Air.lan>
2020-04-07 17:56:14 -0400

    Updates to implementation

--- src/execute.sh
+++ src/execute.sh
@@ -4,7 +4,7 @@
 # 1. Delete core-site.xml contents --> cd ~/usr/local/hadoop/etc/hadoop sudo nano core-site.xml
 # 2. Change hostname in config file --> cd ~/.ssh sudo nano config --> public DNS needs changed from Amazon AWS instance
 # 3. Edit hosts file with private IP --> cd ~/usr/local/hadoop/etc/hadoop sudo nano hosts --> private IP followed by namenode or datanode
-# 4. Remove localhost keygen from known hosts --> cd ~/.ssh ssh-keygen -f "home/ubuntu/.ssh/known_hosts" -R "localhost"
+# 4. Remove localhost keygen from known hosts --> cd ~/.ssh ssh-keygen -f "/home/ubuntu/.ssh/known_hosts" -R "localhost"
 # 5. Create new keygen with new IP address --> cd ~/.ssh ssh-keygen -t rsa (press enter when prompted, and 'y' to overwrite)
 # 6. Move new keygen to authorized keys --> cd ~/.ssh cat id_rsa.pub >> authorized_keys
 # 7. Create input file in correct directory --> cd ~/usr/local/hadoop sudo nano input (or for bigger files scp -r filename ubuntu@ip:/home/ubuntu)
@@ -28,7 +28,7 @@ echo "Starting Yarn Files: "
 ./start-yarn.sh # start the yarn files which include resource manager and node managers #
 echo "Running MapReduce Job: "
 cd ..
-bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.jar wordcount input output # runs the MapReduce job #
+bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.1.jar wordcount enwik9 output # runs the MapReduce job #
 # be sure to create input file manually using sudo nano input and put data in here before running job #
 echo "Displaying Output of Running MapReduce Job: "
 bin/hdfs dfs -cat output/* # displays the output found in the hadoop distributed file system stored in the output file as declared in previous step #
@@ -40,11 +40,10 @@ duration=$(( SECONDS - start )) # end timer #
 echo "Printing Duration: "
 echo $duration
 echo "Stopping timer: "
-total_time=300+$duration
+echo "Total Time Including Provisioning of Instances and Execution Time: "
+provisioningTime=300
+total_time=$(( $provisioningTime + $duration ))
 echo $total_time
 echo "Deleting Output File to Avoid Overlap"
 rm -R output
-# cd ..
-# cd ..
-# cd ~/usr/local/hadoop/sbin
-# ./stop-all.sh
+echo "Thank you for using this tool! Goodbye!"
